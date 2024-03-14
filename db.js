31bf30cdc12b85885f8c8d7844a7f9a48239c1e7
Jonathan0008 <s22110362@student.unklab.ac.id>
2024-03-14 07:06:44 +0700

    changing

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'Mel123',
+  password: 'ostgres',
   host: 'localhost',
   port: 5432, // default Postgres port
   database: 'exercise7'
